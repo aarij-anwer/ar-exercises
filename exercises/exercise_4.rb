483cd3268a3380d05d3a77bde90cb455087a9833
Aarij Anwer <aarij.anwer@gmail.com>
2023-04-28 20:07:10 -0400

    loading subset of stores for men and women

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,36 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.new
+store.name = "Surrey"
+store.annual_revenue = "224000"
+store.mens_apparel = "false"
+store.womens_apparel = "true"
+store.save
+
+store = Store.new
+store.name = "Whistler"
+store.annual_revenue = "1900000"
+store.mens_apparel = "true"
+store.womens_apparel = "false"
+store.save
+
+store = Store.new
+store.name = "Yaletown"
+store.annual_revenue = "430000"
+store.mens_apparel = "true"
+store.womens_apparel = "true"
+store.save
+
+@men_stores = Store.where(mens_apparel: "true")
+
+@men_stores.each do |store|
+  puts "#{store.name}, $#{store.annual_revenue}"
+end
+
+@women_stores = Store.where('annual_revenue < ? AND womens_apparel = ?', 1000000, 'true')
+
+@women_stores.each do |store|
+  puts "#{store.name}, $#{store.annual_revenue}"
+end
