eedd031f6d116948d208046ec9b934db34d0a191
Aarij Anwer <aarij.anwer@gmail.com>
2023-04-28 20:26:57 -0400

    create associations and seed data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: '1')
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1 = Store.find_by(id: '1')
+@store1.employees.create(first_name: "Aarij", last_name: "Anwer", hourly_rate: 60)
+@store1 = Store.find_by(id: '2')
+@store1.employees.create(first_name: "Atiqa", last_name: "Ahmed", hourly_rate: 60)
+@store1 = Store.find_by(id: '2')
+@store1.employees.create(first_name: "Sairish", last_name: "Ahmed", hourly_rate: 60)
