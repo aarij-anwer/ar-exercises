a9f5722ca48963e6d6de4e6a234c0f4af9165e44
Aarij Anwer <aarij.anwer@gmail.com>
2023-04-28 20:17:32 -0400

    add calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_price = Store.sum(:annual_revenue)
+puts "Total revenue: $#{total_price}"
+
+total_stores = Store.count
+puts "Average revenue: $#{total_price/total_stores}"
+
+milli_stores = Store.where('annual_revenue >= ?', 1000000).count
+puts "Million dollar stores: #{milli_stores}"
